f9edd83783a6ccf6e56998c0f1d016ea3c219166
Lin014 <ciling000@gmail.com>
2023-04-18 20:54:59 +0800

    update logo

--- src/constants.js
+++ src/constants.js
@@ -3,4 +3,3 @@ export const API_GET_HackMDData = `${API_HOST}/`
 export const API_ADD_HackMDData = `${API_HOST}/add`
 export const API_UPDATE_HackMDData = `${API_HOST}/update`
 export const API_DELETE_HackMDData = `${API_HOST}/delete`
-
